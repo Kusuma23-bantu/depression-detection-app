469a53d1884c4b22c5caa65535b8c2c51fcbc5ba
Kusuma23-bantu <bantukusuma@gmail.com>
2025-07-07 23:38:02 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from predict import predict_depression
+from model.predict import predict_depression
  # ✅ using enhanced logic
 
 app = Flask(__name__)
